a78babf85bdc74f5c8bc853f1634db4d59ef44c8
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-03-24 15:57:39 +0000

    Create login and register views

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,6 +4,22 @@ function homePage(req: Request, res: Response) {
   res.render("index");
 }
 
+function getLogin(req: Request, res: Response) {
+  res.render("login");
+}
+
+function getRegister(req: Request, res: Response) {
+  res.render("register");
+}
+
+async function postLogin(req: Request, res: Response) {}
+
+async function postRegister(req: Request, res: Response) {}
+
 export default {
   homePage,
+  getLogin,
+  getRegister,
+  postLogin,
+  postRegister,
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,9 @@ const router = express.Router();
 
 router.get("/", controller.homePage);
 
+router.get("/login", controller.getLogin);
+router.get("/register", controller.getRegister);
+router.post("/login", controller.postLogin);
+router.post("/regiser", controller.postRegister);
+
 export default router;
